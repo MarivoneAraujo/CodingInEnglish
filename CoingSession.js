b3cd796169d975226ad1c6d79b5fe02c9d56d18a
Marivone V. Araujo <marivone.vieira@gmail.com>
2023-08-06 16:52:08 -0300

    Code

---
+++ CoingSession.js
@@ -0,0 +1,48 @@
+/* Let's practice our coding skills
+Use any programming language 
+
+1. Algorithm 1: Reverse a String
+Write a JavaScript function that takes a string input and returns the reversed version of the string.
+ For example, if the input is "hello", the function should return "olleh".
+
+ 2. Algorithm 2: Find the Longest Word
+Write a JavaScript function that takes a string input and returns the length of the longest word in the string. 
+For example, if the input is "The quick brown fox jumped over the lazy dog", the function should return 6 (for the word "jumped").
+
+3. Algorithm 3: Counting Vowels
+Write a JavaScript function that takes a string input and returns the number of vowels (a, e, i, o, u) in the string. 
+For example, if the input is "hello world", the function should return 3.
+
+4. Algorithm 4: Palindrome Checker
+Write a JavaScript function that takes a string input and returns true if the string is a palindrome (reads the same backward as forward), 
+and false otherwise. For example, if the input is "racecar", the function should return true.
+
+5. Algorithm 5: Find the Missing Number
+Write a JavaScript function that takes an array of numbers from 1 to n, where one number is missing, 
+and returns the missing number. For example, if the input is [1, 2, 3, 5, 6], the function should return 4. 
+
+6. Algorithm 6: Find the First Non-Repeated Character
+Write a JavaScript function that takes a string input and returns the first non-repeated (unique) character in the string. 
+For example, if the input is "aabbccdeeff", the function should return "d".
+
+7. Algorithm 7: Capitalize Words
+Write a JavaScript function that takes a string input and capitalizes the first letter of each word in the string. 
+For example, if the input is "hello world", the function should return "Hello World".
+
+8. Algorithm 8: Remove Duplicates from an Array
+Write a JavaScript function that takes an array of numbers or strings and returns a new array with duplicates removed. 
+For example, if the input is [1, 2, 3, 2, 1, 4, 5, 4], the function should return [1, 2, 3, 4, 5].
+
+9. Algorithm 9: Sum of Two Numbers
+Write a JavaScript function that takes an array of numbers and a target sum, and returns a pair of numbers from the array 
+that add up to the target sum. For example, if the input is [2, 3, 5, 7, 9] and the target sum is 10, the function should return [3, 7].
+
+10. Algorithm 10: Check for Balanced Brackets
+Write a JavaScript function that takes a string containing brackets (e.g., "{}, [], ()") and returns true if the brackets are balanced, 
+and false otherwise. For example, if the input is "{[()]}", the function should return true, but if the input is "{[()}", 
+the function should return false. 
+
+11. FizzBuzz:
+Write a function that takes a number as input and returns "Fizz" if the number is divisible by 3,
+"Buzz" if divisible by 5, and "FizzBuzz" if divisible by both 3 and 5. Otherwise, return the 
+input number. For example, if the input is 15, the output should be "FizzBuzz." */
